4cd736bcddafee11d2213efecf2665bee228421a
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-08-05 01:17:29 +0200

    aggiunge event listener relativi al cambio colore background dei selettori  marker nel dialog

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const game = (function (){
     let turn;
     let gameOver = false;
     //factory function per la creazione dei giocatori
-    function createPlayer(name, mark, color){
+    function createPlayer(name, mark, color, markerImg){
         let score = 0;
         
         
@@ -93,10 +93,18 @@ const game = (function (){
             return color;
         }
 
+        const setImg = function(newImg){
+            markerImg = newImg;
+        }
+
+        const getImg = function(){
+            return markerImg;
+        }
 
 
 
-        return {getName, setName, setMark, getMark, modifyScore, resetScore, getScore, getColor, setColor};
+        return {getName, setName, setMark, getMark, modifyScore, resetScore, getScore, getColor,
+             setColor, setImg, getImg};
     }
 
     //funzione che avvia la partita
@@ -177,32 +185,42 @@ const game = (function (){
 const DisplayHandler = (function(){
 
 //Ricerca elementi nel Dom:
-
-    //elementi player1
-    const editPlayer1 = document.querySelector("#p1Button");
+    //elementi generici:
     const startButton = document.querySelector("#startButton");
     const gameBoardDiv = document.querySelector("#gameBoard");
+    //elementi player1
+    const editPlayer1 = document.querySelector("#p1Button");
     const p1Dialog = document.querySelector("#p1Dialog");
-    const p1closeButton = document.querySelector("#p1CloseButton");
+    const p1CloseButton = document.querySelector("#p1CloseButton");
     const p1Form = document.querySelector("#p1Form")
     const p1NameInput = document.querySelector("#player1NameInput");
     const p1ColorInput = document.querySelector("#p1ColorInput");
     const p1Score = document.querySelector("#p1Score")
+    const p1FormContainer = document.querySelector("#p1FormContainer");
     //elementi player2
     const p2CloseButton = document.querySelector("#p2CloseButton");
     const p2Dialog = document.querySelector("#p2Dialog");
     const editPlayer2 = document.querySelector("#p2Button");
-    const p2Score = document.querySelector("#p2Score")
+    const p2Score = document.querySelector("#p2Score");
+    const p2NameInput = document.querySelector("#player2NameInput");
+    const p2ColorInput = document.querySelector("#p2ColorInput");
+    const p2Form = document.querySelector("#p2Form");
+    const p2FormContainer = document.querySelector("#p2FormContainer");
     
     //settaggi iniziali:
     p1Score.style.color =game.player1.getColor();
     p2Score.style.color =game.player2.getColor();
-    //eventListeners:
+    game.player1.setMark("url(./icons/close.png)");
+    game.player2.setMark("url(./icons/rec.png)")
+    
+    
+    //eventListeners
+    
     //finestre modali:
     editPlayer1.addEventListener("click", () => p1Dialog.showModal());
     editPlayer2.addEventListener("click", ()=> p2Dialog.showModal());
     //close buttons:
-    p1closeButton.addEventListener("click", (e)=>{
+    p1CloseButton.addEventListener("click", (e)=>{
         e.preventDefault();
         if(!p1Form.checkValidity()){
             p1Form.reportValidity();
@@ -215,7 +233,92 @@ const DisplayHandler = (function(){
             p1Dialog.close();
            
         }
-    })
+    });
+
+    p2CloseButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        if(!p2Form.checkValidity()){
+            p2Form.reportValidity();
+        }else{
+            game.player2.setName(p2NameInput.value);
+            game.player2.setColor(p2ColorInput.value);
+            p2Score.textContent = `${game.player2.getName()}: ${game.player2.getScore()} `;
+            p2Score.style.color = game.player2.getColor();
+
+            p2Dialog.close();
+           
+        }
+    });
+
+    //selettore colore:
+    p1ColorInput.addEventListener("input", ()=>{
+        game.player1.setColor(p1ColorInput.value);
+        console.log(`cambioColore ${game.player1.getColor()}`);
+
+    });
+    p2ColorInput.addEventListener("input", ()=>{
+        game.player2.setColor(p2ColorInput.value);
+        console.log(`cambioColore ${game.player2.getColor()}`);
+
+    });
+
+    //selettori marker giocatori:
+    p1FormContainer.addEventListener("click", (e)=>{
+        let target = e.target;
+        switch(target.id){
+            case "p1Mark1Img":
+                document.querySelectorAll(".markImg").forEach(element =>{
+                    element.style.backgroundColor = "#ffffff";
+                });
+                target.style.backgroundColor = game.player1.getColor();
+                console.log("click");
+                break;
+            case "p1Mark2Img":
+                document.querySelectorAll(".markImg").forEach(element =>{
+                    element.style.backgroundColor = "#ffffff";
+                });
+                target.style.backgroundColor = game.player1.getColor();
+                console.log("click");
+                break;
+            case  "p1Mark3Img":   
+            document.querySelectorAll(".markImg").forEach(element =>{
+                element.style.backgroundColor = "#ffffff";
+            });
+            target.style.backgroundColor = game.player1.getColor();
+            console.log("click");
+            break;
+
+        }
+    });
+
+    p2FormContainer.addEventListener("click", (e)=>{
+        let target = e.target;
+        switch(target.id){
+            case "p2Mark1Img":
+                document.querySelectorAll(".markImg").forEach(element =>{
+                    element.style.backgroundColor = "#ffffff";
+                });
+                target.style.backgroundColor = game.player2.getColor();
+                console.log("click");
+                break;
+            case "p2Mark2Img":
+                document.querySelectorAll(".markImg").forEach(element =>{
+                    element.style.backgroundColor = "#ffffff";
+                });
+                target.style.backgroundColor = game.player2.getColor();
+                console.log("click");
+                break;
+            case  "p2Mark3Img":   
+            document.querySelectorAll(".markImg").forEach(element =>{
+                element.style.backgroundColor = "#ffffff";
+            });
+            target.style.backgroundColor = game.player2.getColor();
+            console.log("click");
+            break;
+
+        }
+    });
+
 
 
 
